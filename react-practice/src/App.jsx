0a6ea355715a34fc7b7da9cb8eba7fabd416721c
Parth Sharma <parthsh.161106@gmail.com>
2026-07-26 16:35:19 +0530

    MyReactProjects

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,32 +1,12 @@
-import { useState, createContext } from "react";
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Workouts from "./Day5/Workouts";
-import Diet from "./Day5/Diet";
-import Progress from "./Day5/Progress";
-import Contact from "./Day5/Contact";
-import NotFound from "./Day5/NotFound";
-import GymNavbar from "./Day5/GymNavbar";
-import GymLayout from "./Day5/GymLayout";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <GymLayout/>,
-    children: [
-      { path: "/", element: <Workouts/> },
-      { path: "/diet", element: <Diet/> },
-      { path: "/progress", element: <Progress/> },
-      { path: "/contact", element: <Contact/> },
-      { path: "*", element: <NotFound/> }
-    ]
-  }
-]);
+import { useState} from "react";
+import ExpenseSplitter from "./Revision/ExpenseSplitter";
+  
 
 function App() {
   
   return (
     <div>
-      <RouterProvider router={router}/>   
+      <ExpenseSplitter/>
     </div>
   )
 
---
+++ react-practice/src/Revision/ExpenseSplitter.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react';
+import './ExpenseSplitter.css'
+
+const ExpenseSplitter = () => {
+    const[expenses, setExpenses] = useState([]);
+    const[name, setName] = useState("");
+    const[amount, setAmount] = useState("");
+    const [people, setPeople] = useState("1");
+
+    function handleAdd() {
+        if(!name || !amount) {
+            return;
+        }
+        const newExpense = {id: Date.now(), name: name, amount: Number(amount)};
+        setExpenses([...expenses, newExpense]);
+        setName("");
+        setAmount("");
+    }
+    
+    const total = expenses.reduce((sum, exp) => sum + exp.amount, 0);
+  return (
+    <div className='expense-card'>
+      <h1 className='expense-title'>💰 Expense Splitter</h1>
+      <input className = 'expense-input' type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+      <input className = 'expense-input' type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+      <button className='expense-btn' onClick={handleAdd}>Add expense</button>
+      {expenses.length === 0 ? (
+        <p className='expense-empty'>No expenses yet 🪹</p>
+    ) : (
+    <ul className='expense-list'>
+        {expenses.map((exp) => (
+    <li key={exp.id}>{exp.name} — ₹{exp.amount}</li>
+    ))}
+    </ul>
+    )}
+      <h3 className='expense-total'>Total: ₹{total}</h3>
+      <input className = 'expense-input'  type="number" value={people} onChange={(e) => setPeople(e.target.value)} />
+      {people > 0 && <h3>Each person pays: ₹{(total / people).toFixed(2)}</h3>} 
+    </div>
+  )
+}
+
+export default ExpenseSplitter
