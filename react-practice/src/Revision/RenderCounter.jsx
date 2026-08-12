4e0ce839ff37f77add66a8cda04d64abdc5b0bf9
Parth Sharma <parthsh.161106@gmail.com>
2026-08-12 13:24:25 +0530

    ReactPractice

--- react-practice/src/Revision/RenderCounter.jsx
+++ react-practice/src/Revision/RenderCounter.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 const RenderCounter = () => {
 
     const [count, setCount] = useState(0);
     let counterRef = useRef(0);
+    let prevCounterRef = useRef(0);
 
     counterRef.current = counterRef.current + 1;
     console.log("Render #" + counterRef.current);
+
+    useEffect(() => {
+     prevCounterRef.current = count;
+    }, [count])
+    
     
 
   return (
     <div>
-      <p>Count: {count}</p>
+      <p>Now: {count}</p>
+      <p>before: {prevCounterRef.current}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+     
     </div>
   )
 }
